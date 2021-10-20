a18f0a3f4ff2e8875b473f4dec9a94c178d5c2bd
Alexander Sharov <kvendingoldo@yandex.ru>
2021-10-20 04:55:11 +0400

    Pre-merge changes

--- main.go
+++ main.go
@@ -7,16 +7,12 @@ import (
 	"github.com/kvendingoldo/aws-letsencrypt-lambda/internal/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age  int    `json:"How old are you?"`
-}
-
 type MyResponse struct {
 	Message string `json:"Answer:"`
 }
 
 func Handler(ctx context.Context) (string, error) {
+	// TODO
 	//config := cfg.New()
 	//lambda.Execute()
 	//return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
